02ad07ec550cd5a35b005a74632e987aed56c027
ashu2305 <ayushgupta2305@gmail.com>
2021-01-16 17:40:10 +0530

    first commit

---
+++ blog-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from "./Components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
